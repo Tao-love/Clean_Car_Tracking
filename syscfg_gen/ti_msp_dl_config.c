0dae162f1001093dc3f59ae57ba7ed432a8d8642
Hu_Tao <33647658@qq.com>
2026-07-20 21:07:36 +0800

    chore: make teammate project reproducible locally

--- syscfg_gen/ti_msp_dl_config.c
+++ syscfg_gen/ti_msp_dl_config.c
@@ -434,12 +434,16 @@ SYSCONFIG_WEAK void SYSCFG_DL_UART_DEBUG_init(void)
     DL_UART_Main_init(UART_DEBUG_INST, (DL_UART_Main_Config *) &gUART_DEBUGConfig);
     /*
      * Configure baud rate by setting oversampling and baud rate divisors.
-     *  Target baud rate: 115200
-     *  Actual baud rate: 115211.52
+     *  Target baud rate: 9600
+     *  Actual baud rate: 9600.24
      */
     DL_UART_Main_setOversampling(UART_DEBUG_INST, DL_UART_OVERSAMPLING_RATE_16X);
-    DL_UART_Main_setBaudRateDivisor(UART_DEBUG_INST, UART_DEBUG_IBRD_32_MHZ_115200_BAUD, UART_DEBUG_FBRD_32_MHZ_115200_BAUD);
+    DL_UART_Main_setBaudRateDivisor(UART_DEBUG_INST, UART_DEBUG_IBRD_32_MHZ_9600_BAUD, UART_DEBUG_FBRD_32_MHZ_9600_BAUD);
 
+
+    /* Configure Interrupts */
+    DL_UART_Main_enableInterrupt(UART_DEBUG_INST,
+                                 DL_UART_MAIN_INTERRUPT_RX);
 
     /* Configure FIFOs */
     DL_UART_Main_enableFIFOs(UART_DEBUG_INST);
